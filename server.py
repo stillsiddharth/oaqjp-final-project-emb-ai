78fcaf47fad90aa07e4a580a0cd80c5e753f99a5
stillsiddharth <siddharthkanjaria@outlook.com>
2026-08-02 20:15:31 -0400

    Add error handling for emotion detection and server

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def emo_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
